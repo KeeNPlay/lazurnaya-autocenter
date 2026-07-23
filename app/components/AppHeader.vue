048ef0bb04ef71c34879e608d0ce0ecf74353508
KeeNPlay <nikita.kotegov@mail.ru>
2026-07-23 09:07:02 +0300

    feat(ui): created component AppHeader

---
+++ app/components/AppHeader.vue
@@ -0,0 +1,7 @@
+<template>
+    <header class="fixed inset-x-0 top-0 z-10 px-40.5 py-5 border-b box-border border-gray-150 ">
+        <div class="flex items-center justify-between">
+            
+        </div>
+    </header>
+</template>
