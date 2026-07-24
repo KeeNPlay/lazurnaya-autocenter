6e10a9262e5a5314a77b03de79c548e2afe442ee
KeeNPlay <nikita.kotegov@mail.ru>
2026-07-24 09:02:28 +0300

    fix(ui): added styles to support large screens

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -1,5 +1,5 @@
 <template>
-    <header class="fixed inset-x-0 top-0 z-10 px-20 py-5 border-b box-border border-gray-150">
+    <header class="fixed inset-x-0 top-0 z-10 px-20 py-5 border-b box-border border-gray-150 2xl:px-40.5">
         <div class="flex items-center justify-between">
             <AppLogo>АВТОЦЕНТР НА ЛАЗУРНОЙ</AppLogo>
             <AppHeaderLinks />
--- app/components/AppLogo.vue
+++ app/components/AppLogo.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="flex items-center">
-        <div class="p-2.5 bg-orange-500 rounded-[10px]">
-            <Icon name="custom-icons:app-logo" class="text-[2.5rem] text-black"/>
+        <div class="p-1.75 bg-orange-500 rounded-[5px] 2xl:rounded-[10px]">
+            <Icon name="custom-icons:app-logo" class="text-[1.25rem] text-black 2xl:text-[2.5rem]"/>
         </div>
-        <span v-if="$slots.default" class="text-lg font-bold ml-3"><slot /></span>
+        <span v-if="$slots.default" class="text-sm ml-1.75 font-bold 2xl:text-lg 2xl:ml-3"><slot /></span>
     </div>
 </template>
