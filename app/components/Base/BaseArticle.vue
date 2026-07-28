57209c794b4e65a957dbef3d592ea5f09f11942a
KeeNPlay <nikita.kotegov@mail.ru>
2026-07-28 09:02:26 +0300

    feat(ui): added variants and semantic tags for BaseArticle component

--- app/components/Base/BaseArticle.vue
+++ app/components/Base/BaseArticle.vue
@@ -1,5 +1,30 @@
+<script setup lang="ts">
+type Variant = 'primary' | 'badge';
+type Tag = 'span' | 'div' | 'p' | 'mark'
+    
+interface IProps {
+    variant?: Variant
+    as?: Tag
+}
+
+const props = withDefaults(defineProps<IProps>(), {
+    variant: 'primary',
+    as: 'p',
+})
+
+const variants: Record<Variant, string> = {
+  primary:
+    'px-3.5 py-3 bg-gray-150 rounded-md 2xl:rounded-lg text-white',
+  badge:
+    'px-6 py-3.5 2xl:px-8.5 2xl:py-5 font-medium text-xs sm:text-base 2xl:text-lg dark:text-white rounded-full bg-gray-100 border border-gray-150',
+}
+</script>
+
 <template>
-    <span class="px-3.5 py-3 bg-gray-150 rounded-lg text-white">
+    <component
+        :class="variants[props.variant]"
+        :is="as"
+    >
         <slot />
-    </span>
+    </component>
 </template>
