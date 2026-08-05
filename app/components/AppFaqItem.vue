f42c818c8d953d012a7c7fcb8e1c6f05bd6bf3ca
KeeNPlay <nikita.kotegov@mail.ru>
2026-08-05 10:01:56 +0300

    fix(ui): fixed Faq Section - styles, animations, icons

--- app/components/AppFaqItem.vue
+++ app/components/AppFaqItem.vue
@@ -17,17 +17,17 @@ defineProps<IProps>()
             <BaseIconBlock class="flex justify-center items-center w-13 h-13 lg:w-16 lg:h-16 2xl:w-22 2xl:h-22">
                 <span class="text-lg lg:text-2xl 2xl:text-3xl font-semibold transition-colors duration-200 text-white group-hover:text-orange-700 group-open:text-orange-500">{{ String(number).padStart(2, '0') }}</span>
             </BaseIconBlock>
-            <div class="flex justify-between items-center lg:items-start lg:pt-6 w-full"> 
+            <div class="flex justify-between items-center w-full"> 
                 <h3 class="text-white group-open:text-orange-700 font-medium text-sm lg:text-lg 2xl:text-2xl transition-colors duration-200">
                     {{ question }}
                 </h3>
                 <Icon
                     name="heroicons:plus-20-solid"
-                    class="text-white text-2xl transition-[transfrom,color] duration-200 group-open:text-orange-700 group-open:rotate-45" 
+                    class="text-white min-w-4 min-h-4 md:min-w-5 md:min-h-5 transition-all duration-200 group-open:text-orange-700 group-open:rotate-45" 
                 />
             </div>
         </summary>
-        <div class="pl-0 lg:pl-21.5 2xl:pl-28 pt-3 lg:pt-0 text-gray-900 text-sm lg:text-base font-light leading-relaxed transition-transform duration-500 group-open:opacity-100 group-open:translate-y-0 -translate-y-5">
+        <div class="pl-0 lg:pl-21.5 2xl:pl-28 pt-3 lg:pt-0 text-gray-900 text-sm lg:text-base font-light leading-relaxed">
             <template v-for="(part, i) in answer" :key="i">
                 <NuxtLink
                     v-if="part.type === 'link'"
