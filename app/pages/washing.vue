5474bc25ed60cf8dbfed792db6c279064acf44af
KeeNPlay <nikita.kotegov@mail.ru>
2026-08-12 11:24:56 +0300

    feat(ui): created Washing page

--- app/pages/washing.vue
+++ app/pages/washing.vue
@@ -1,3 +1,23 @@
+<script setup lang="ts">
+
+import { washingItems } from '~/constants/washingAdvantages'
+
+useSeoMeta({
+  title: 'Мойка — Автоцентр на Лазурной',
+  description: 'Оказываем услуги комплексной мойки грузовых автомобилей, автобусов, прицепов и спецтехники с учетом особенностей каждого типа транспорта.',
+})
+
+</script>
+
 <template>
-    WASHING PAGE
+    <BasePageDescriptionSection
+        header-title="Мойка"
+        header-description="Оказываем услуги комплексной мойки грузовых автомобилей, автобусов, прицепов и спецтехники с учетом особенностей каждого типа транспорта"
+        title="Забота о вашем транспорте в каждой детали"
+        description="Используем профессиональное оборудование и специализированные моющие средства, обеспечивающие эффективное удаление загрязнений, дорожных реагентов и технических отложений."
+        description-badge="Причины работать именно с нами:"
+        image="advantages"
+        :items="washingItems"
+    />
+    <FormsBookingSection default-tab="washing" />
 </template>
